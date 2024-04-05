4f8d2317543a6dd4b882da68cc8f7db99b17dc6f
levi <cgfeel@gmail.com>
2024-04-05 17:12:11 +0800

    完成解构数据

---
+++ src/components/destructor/FieldJsx.tsx
@@ -0,0 +1,50 @@
+import { DatePicker, Radio } from "@formily/antd-v5";
+import { createForm } from "@formily/core";
+import { Field } from "@formily/react";
+import { FC } from "react";
+import FormItem from "../formItem/form-item";
+import Panel from "./Panel";
+
+const form = createForm();
+
+const FieldJsx: FC = () => (
+    <Panel
+        form={form}
+        header={
+            <h2>
+                通过 <code>Field Jsx</code> 解构数据
+            </h2>
+        }>
+        <Field
+            initialValue={true}
+            name="visible_destructor"
+            title="是否显示解构字段"
+            component={[Radio.Group]}
+            dataSource={[
+                { label: "是", value: true },
+                { label: "否", value: false },
+            ]}
+            decorator={[FormItem]}
+        />
+        <Field
+            name="undestructor"
+            title="解构前"
+            component={[DatePicker.RangePicker]}
+            decorator={[FormItem]}
+            required
+        />
+        <Field
+            initialValue={["2020-11-20", "2021-12-30"]}
+            name="[startDate,endDate]"
+            title="解构后"
+            component={[DatePicker.RangePicker]}
+            decorator={[FormItem]}
+            reactions={field => {
+                field.visible = !!field.query("visible_destructor").value();
+            }}
+            required
+        />
+    </Panel>
+);
+
+export default FieldJsx;
--- src/page/Destructor.tsx
+++ src/page/Destructor.tsx
@@ -1,9 +1,13 @@
 import { FC } from "react";
+import FieldJsx from "../components/destructor/FieldJsx";
+import JsonSchema from "../components/destructor/JsonSchema";
 import MarkupSchema from "../components/destructor/MarkupSchema";
 
 const Destructor: FC = () => (
     <>
         <MarkupSchema />
+        <JsonSchema />
+        <FieldJsx />
     </>
 );
 
