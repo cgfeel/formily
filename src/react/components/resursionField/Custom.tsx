dd353d5b7995f167d77532790ca094440368f623
levi <cgfeel@gmail.com>
2024-04-22 19:48:05 +0800

    update

--- src/react/components/resursionField/Custom.tsx
+++ src/react/components/resursionField/Custom.tsx
@@ -1,7 +1,7 @@
 import { ISchema, RecursionField } from "@formily/react";
 import { FC } from "react";
 
-// onlyRenderProperties：是否只渲染properties..
+// onlyRenderProperties：是否只渲染properties
 const Custom: FC<CustomProps> = ({ schema }) => <RecursionField schema={schema} onlyRenderProperties />;
 
 export interface CustomProps {
