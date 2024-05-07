669132764107b420669cec4295c928140376c44e
levi <cgfeel@gmail.com>
2024-05-07 20:03:40 +0800

    更新字段

--- src/__tests__/react/SchemaComs.tsx
+++ src/__tests__/react/SchemaComs.tsx
@@ -69,7 +69,7 @@ export const VoidComponent: FC<PropsWithChildren> = ({ children }) => (
     <div data-testid="void-component">{children}</div>
 );
 
-export interface MarkupProps extends Pick<ISchemaFieldProps, "components" | "name" | "scope"> {
+export interface MarkupProps extends Pick<ISchemaFieldProps, "components" | "name" | "schema" | "scope"> {
     form: Form;
 }
 
--- src/__tests__/react/schema.json.spec.tsx
+++ src/__tests__/react/schema.json.spec.tsx
@@ -1,5 +1,6 @@
 import { createForm } from "@formily/core";
-import { FormProvider, createSchemaField } from "@formily/react";
+import { FormProvider, Schema, createSchemaField } from "@formily/react";
+import { render } from "@testing-library/react";
 import { FC, PropsWithChildren } from "react";
 import { Input, MarkupProps } from "./SchemaComs";
 
@@ -14,7 +15,18 @@ describe("json schema field", () => {
             <SchemaField {...props}>{children}</SchemaField>
         </FormProvider>
     );
+
+    // 字符字段，注意：这里模型是一个字段，而不是整个 ISchema 字段，所以必须给 SchemaField 一个 name 才能正常渲染
     test("string field", () => {
         const form = createForm();
+        const schema = new Schema({
+            default: "123",
+            type: "string",
+            "x-component": "Input",
+        });
+
+        const { queryByTestId } = render(<Markup name="string" form={form} schema={schema} />);
+        expect(queryByTestId("input")).toBeVisible();
+        expect(queryByTestId("input")?.getAttribute("value")).toEqual("123");
     });
 });
