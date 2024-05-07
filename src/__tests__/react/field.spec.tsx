bc70d61b92bd5146b61faaebf83825c51c7046ab
levi <cgfeel@gmail.com>
2024-05-08 00:44:13 +0800

    update

--- src/__tests__/react/field.spec.tsx
+++ src/__tests__/react/field.spec.tsx
@@ -1,6 +1,6 @@
 import { createForm } from "@formily/core";
 import { ArrayField, Field, FormProvider, ObjectField, VoidField, useField } from "@formily/react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import { FC } from "react";
 import { Input as InputRaw, InputProps, TextComponent } from "./SchemaComs";
 
@@ -9,6 +9,7 @@ const Input: FC<Omit<InputProps, "testid">> = props => {
     return <InputRaw {...props} testid={field.path.toString()} />;
 };
 
+// 渲染字段
 test("render field", async () => {
     const form = createForm();
     const onChange = jest.fn();
@@ -42,4 +43,27 @@ test("render field", async () => {
         </FormProvider>,
     );
     expect(form.mounted).toBeTruthy();
+    expect(form.query("aa").take()?.mounted).toBeTruthy();
+    expect(form.query("bb").take()?.mounted).toBeTruthy();
+    expect(form.query("cc").take()?.mounted).toBeTruthy();
+    expect(form.query("dd").take()?.mounted).toBeTruthy();
+
+    fireEvent.change(getByTestId("aa"), {
+        target: { value: "123" },
+    });
+
+    // 字段 kk 没有调用监听函数，不会触发执行
+    fireEvent.change(getByTestId("kk"), {
+        target: { value: "123" },
+    });
+
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(getByTestId("bb-children")).toBeVisible();
+    expect(getByTestId("dd-children")).toBeVisible();
+
+    // 被隐藏的字段需要通过 queryByTestId，使用 getByTestId 获取节点找不到会抛出错误
+    expect(queryByTestId("ee")).toBeNull();
+    expect(form.query("aa").get("value")).toEqual("123");
+    expect(form.query("kk").get("value")).toEqual("123");
+    unmount();
 });
