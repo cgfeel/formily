16269933e4b30acb7d0d8db35db081757d325425
levi <cgfeel@gmail.com>
2024-05-08 23:22:51 +0800

    完成 field

--- src/__tests__/react/field.spec.tsx
+++ src/__tests__/react/field.spec.tsx
@@ -1,4 +1,4 @@
-import { createForm, Field as FieldType, isArrayField, isField, onFieldChange } from "@formily/core";
+import { createForm, Field as FieldType, isArrayField, isField, onFieldChange, onFieldUnmount } from "@formily/core";
 import {
     ArrayField,
     Field,
@@ -45,6 +45,7 @@ const CustomField = connect(
         ...props,
         mounted: field.mounted ? 1 : 2,
     })),
+    // initialValue 在这里丢失了
     mapReadPretty(({ initialValue, value }) => (
         <div data-testid="custom-read">
             read pretty {initialValue || "text-component"} {value}
@@ -55,12 +56,24 @@ const CustomField = connect(
 const Input = connect(
     InputRaw,
     mapProps((props, field) => ({ ...props, testid: field.path.toString() })),
+    // testid 在这里丢失了
     mapReadPretty(({ testid, value }) => (
         <div data-testid="input-read">
             {value}-{testid}
         </div>
     )),
 );
+
+// 只要是 value 后面的属性一定是字段值，不用被 type 这样的名字唬到
+// 这里的意思是如果当前的字段值下有个 type 为 mouted 的值，就嵌套一个子组件
+const ParentChild: FC = () => {
+    const field = useField<FieldType>();
+    return field.value?.type === "mouted" ? (
+        <Field name="child" component={[Input]} validator={{ required: true }} />
+    ) : (
+        <div data-testid="unmounted"></div>
+    );
+};
 
 // 渲染字段
 test("render field", async () => {
@@ -207,6 +220,8 @@ test("useFormEffects", async () => {
     });
 });
 
+// 第三方组件库的无侵入接入 Formily
+// 由于其他单例已经创建了组件，这里就直接使用接入为新的组件，这样更符合实际开发场景
 test("content", async () => {
     const form = createForm();
     const MyComponent: FC = () => (
@@ -243,5 +258,104 @@ test("content", async () => {
     await waitFor(() => {
         expect(queryByTestId("custom-read")?.innerHTML).toEqual("read pretty text-component custom-field");
         expect(queryByTestId("input-read")?.innerHTML).toEqual("123-");
+    });
+});
+
+// 字段验证和卸载
+test("field unmount and validate", async () => {
+    const fn = jest.fn();
+    const Parent = observer(ParentChild);
+
+    // 创建一个带有初始值的表单，初始值字段为 { parent: { type: "mouted" } }
+    const form = createForm({
+        initialValues: {
+            parent: {
+                type: "mouted",
+            },
+        },
+        effects: () => {
+            onFieldUnmount("parent.child", fn);
+        },
+    });
+
+    // 这里用组件 Parent 能够说明 Field 是可以层层嵌套的
+    const MyComponent: FC = () => (
+        <FormProvider form={form}>
+            <div data-testid="container">
+                <Field name="parent" component={[Parent]} />
+            </div>
+        </FormProvider>
+    );
+
+    const { queryByTestId } = render(<MyComponent />);
+    try {
+        await form.validate();
+    } catch {}
+
+    // 表单不合理的原因是因为 child 这个必填字段成功挂载，而触发了验证不通过
+    expect(form.invalid).toBeTruthy();
+    expect(form.query("parent.child").take()?.mounted).toBeTruthy();
+    expect(fn).toHaveBeenCalledTimes(0);
+
+    // 这个时候 input 还是存在的
+    expect(queryByTestId("parent.child")).toBeVisible();
+
+    act(() => {
+        form.query("parent").take(field =>
+            field.setState(state => {
+                state.value.type = "unmounted";
+            }),
+        );
+    });
+
+    // 更改值后，组件被卸载
+    await waitFor(() => {
+        expect(fn).toHaveBeenCalledTimes(1);
+    });
+
+    try {
+        await form.validate();
+    } catch {}
+
+    // 这件事告诉我们即便
+    expect(form.invalid).toBeTruthy();
+
+    // 巩固，字段节点卸载后并不会影响表单验证，也不影响表单中已经存在卸载字段的事实
+    expect(queryByTestId("parent.child")).toBeNull();
+    expect(queryByTestId("unmounted")).toBeVisible();
+
+    // 如果要彻底清除字段，可以隐藏字段
+    act(() => {
+        form.query("parent.child").take(field =>
+            field.setState(state => {
+                state.display = "none";
+            }),
+        );
+    });
+
+    try {
+        await form.validate();
+    } catch {}
+
+    await waitFor(() => {
+        expect(form.invalid).toBeFalsy();
+    });
+
+    // 如果要彻底清除字段，可以通过回收字段的方式
+    act(() => {
+        form.query("parent.child").take(field =>
+            field.setState(state => {
+                state.display = "visible";
+            }),
+        );
+        form.clearFormGraph("*");
+    });
+
+    try {
+        await form.validate();
+    } catch {}
+
+    await waitFor(() => {
+        expect(form.invalid).toBeFalsy();
     });
 });
