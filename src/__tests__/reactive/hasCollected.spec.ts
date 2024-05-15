ffab2472d7055cc1d23b677220bd6eacb1762af9
levi <cgfeel@gmail.com>
2024-05-15 16:17:43 +0800

    update

---
+++ src/__tests__/reactive/hasCollected.spec.ts
@@ -0,0 +1,19 @@
+import { autorun, hasCollected, observable } from "@formily/reactive";
+
+// 检测某段执行逻辑是否存在依赖收集
+test("hasCollected", () => {
+    const obs = observable({ value: "" });
+    autorun(() => {
+        // 在 autorun 中会自动收集依赖
+        expect(hasCollected(() => { obs.value })).toBeTruthy();
+
+        // 在 autorun 中不提供劫持对象，无法正常收集
+        expect(hasCollected(() => {})).toBeFalsy();
+        expect(hasCollected()).toBeFalsy();
+    });
+
+    // 在 autorun 外部不能自动收集依赖
+    expect(hasCollected(() => { obs.value })).toBeFalsy();
+    expect(hasCollected(() => {})).toBeFalsy();
+    expect(hasCollected()).toBeFalsy();
+});
