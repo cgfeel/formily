0cb44f87fea711bf19868e28e7c2a853b97b1754
levi <cgfeel@gmail.com>
2024-05-15 19:44:22 +0800

    update

---
+++ src/__tests__/reactive/untracked.spec.ts
@@ -0,0 +1,24 @@
+import { autorun, observable, untracked } from "@formily/reactive";
+
+// untracker 函数内部永远不会被依赖收集
+test("basic untracked", () => {
+    const obs = observable<{ value?: number }>({});
+    const handler = jest.fn();
+
+    autorun(() => {
+        // 被 untracked 包裹后不再收集依赖
+        untracked(() => handler(obs.value));
+    });
+
+    // 初始化调用 1 次
+    expect(handler).toHaveBeenCalledTimes(1);
+
+    // 修改值不会触发响应
+    obs.value = 123;
+    expect(handler).toHaveBeenCalledTimes(1);
+});
+
+// untracked 不提供参数什么也不会发生
+test("no params untracked", () => {
+    untracked();
+});
