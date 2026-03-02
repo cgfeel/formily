fafb1c06f57e922c6db19391875a5fc786f53051
cgfeel <cgfeel@gmail.com>
2026-03-03 02:14:40 +0800

    fix: update ci and lint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   // 匹配所有子包的 js/ts 文件，执行 eslint
-  "packages/**/src/**/*.{js,ts,jsx,tsx}": "eslint --fix --config ./eslint.config.ts",
+  "src/**/*.{js,ts,jsx,tsx}": "eslint --fix --config ./eslint.config.ts",
   // 匹配所有子包的样式文件，执行 stylelint
-  "packages/**/*.{css,less,scss}": "stylelint --fix",
+  "src/**/*.{css,less,scss}": "stylelint --fix",
   // 可选：格式化文件
   "**/*.{js,ts,jsx,tsx,json,md}": "prettier --write",
 };
